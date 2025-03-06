dee33e2875441668420e482f70c64ba57c801f20
Jiggly Balls <jigglyballs9000@gmail.com>
2025-03-06 13:48:40 +0530

    Added examples

--- HelloWorld/src/DataTypes.java
+++ HelloWorld/src/DataTypes.java
@@ -1,5 +1,32 @@
+/*
+Notes-
+    Variables use pascal casing
+    '+' is used for concatenation
+    char datatype value should be in single quotes
+    String datatype should be in double quotes
+ */
+
 public class DataTypes {
     public static void main(String[] args) {
-        System.out.println("Testing");
+        // Primitive Datatypes-
+
+        int age = 19;
+        double salary = -23.4;
+        boolean isUnderage = false;
+        char grade = 'F';
+
+        // Non-Primitive Datatypes-
+
+        String name = "Krish";
+
+        System.out.println(
+                "My name is " + name + ". I'm " + age + " years old "
+                + "and my salary is " + salary
+        );
+
+        System.out.println(
+                "Am I underage? " + isUnderage + "\nMy math grade: " + grade
+        );
+
     }
 }
