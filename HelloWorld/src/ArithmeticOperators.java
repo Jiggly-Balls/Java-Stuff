7b7ab531bb078c68787db1212039cd456e68d9aa
Jiggly Balls <jigglyballs9000@gmail.com>
2025-03-08 18:18:27 +0530

    Added arithmetic operator examples

---
+++ HelloWorld/src/ArithmeticOperators.java
@@ -0,0 +1,16 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 2;
+        System.out.println(num1 + num2); // Addition
+        System.out.println(num1 - num2); // Subtraction
+        System.out.println(num1 * num2); // Multiplication
+        System.out.println(num1 / num2); // Division
+        System.out.println(num1 % num2); // Modulus
+
+        num1++; // Increment
+        num2--; // Decrement
+
+        System.out.println(num1 + " " + num2);
+    }
+}
